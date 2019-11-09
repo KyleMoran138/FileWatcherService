c6ecb509422005f7e45413e294674d91daacb454
Kylemoran138 <Kylemoran.data@gmail.com>
2019-11-08 23:30:42 -0500

    Release 1.0.0 -Update gitignore -Update program.cs -Add a release zip

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace FileWatcher
 {
@@ -11,16 +13,38 @@ namespace FileWatcher
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.File(@"C:\\temp\\fileTrackingWorkerService\\Log.txt")
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting the file watcher service");
+                CreateHostBuilder(args).Build().Run();
+            }catch(Exception ex)
+            {
+                Log.Fatal(ex, "There was a problem starting the service");
+                return;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.Configure<FileWatcherOptions>(hostContext.Configuration.GetSection("fileWatcherOptions"));
                     services.Configure<SmtpEmailClientOptions>(hostContext.Configuration.GetSection("smtpEmailClientOptions"));
                     services.AddHostedService<Worker>();
-                });
+                })
+                .UseSerilog();
     }
 }
